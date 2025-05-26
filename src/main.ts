61fe7af396e0aceef11e91bb3b06bd6d7a677917
Omarkhodjaev <Sirius_2004>
2025-05-26 14:13:39 +0500

    ADD Swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //server port
+  const PORT = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle(`O'quv markaz`)
+    .setDescription(`O'quv markaz ERP api`)
+    .setVersion('1.0')
+    .addServer(`http://localhost:${PORT}`, 'Local Server')
+    .addTag('Users', 'User management')
+    .build();
+
+  const documentOrFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, documentOrFactory);
+
+  await app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}/api-doc`);
+  });
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiTags('Users')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
